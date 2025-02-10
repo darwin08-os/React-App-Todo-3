450d652e1f753183d9bbab286be982a64bfd474b
darwin08-os <harshgajjar7242@gmail.com>
2025-02-11 02:08:35 +0530

    this is mt todo app
    this is my first commit and one functionality is still pending in this app , That is to add task and tommorow i will complete it

---
+++ todo/src/components/TodoItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './TodoList.css'
+export default function TodoItem(props) {
+
+  return (
+    <div className={`TodoItem ${props.task.completed ? "completed" : ""}`}>
+
+        <h3>{props.task.title}</h3>
+        <p>{props.task.completed? "Task Completed Yes" : "Task Completed no"}</p>
+        <button onClick={props.onDelete}>Delete</button>
+        <button onClick={props.onComplete} >Complete</button>
+        
+    </div>
+  )
+}
